14de7dcb7deb497ad4ee843ef8d8886a145dd3a0
sonht <sonht1109@gmail.com>
2022-03-28 08:41:21 +0700

    update tt animation

--- src/components/Tooltip/styles.ts
+++ src/components/Tooltip/styles.ts
@@ -7,7 +7,17 @@ type TooltipStyleProps = Pick<
   "delay" | "offset" | "placement"
 > & { show: boolean } & TooltipPoint;
 
-export const STooltip = styled.span<TooltipStyleProps>`
+const STooltipBase = styled.span<TooltipStyleProps>``;
+
+export const STooltip = styled(STooltipBase).attrs((p: TooltipStyleProps) => {
+  const placement = p.placement || "top";
+  const position = new Position(placement);
+  return {
+    transformOrigin: position.negate(),
+    transformDirection: position.isVertical() ? "translateY" : "translateX",
+    transformSpace: ["top", "left"].includes(placement) ? 4 : -4,
+  };
+})`
   pointer-events: none;
   display: inline-block;
   white-space: nowrap;
@@ -17,18 +27,19 @@ export const STooltip = styled.span<TooltipStyleProps>`
   top: ${({ y }) => y + "px"};
   left: ${({ x }) => x + "px"};
 
+  transform: ${({ transformDirection, transformSpace, show }) =>
+    `${transformDirection}(${show ? 0 : transformSpace}px)`};
+  transition: 0.2s;
+  transition-delay: ${({ delay }) => delay + "ms"};
+  transform-origin: ${({ transformOrigin }) => transformOrigin};
+  opacity: ${({ show }) => (show ? 1 : 0)};
+
   .tool-tip--container {
-    transform: scale(${({ show }) => (show ? 1 : 0.7)});
-    transition: 0.2s;
-    transition-delay: ${({ delay }) => delay + "ms"};
-    transform-origin: ${({ placement }) =>
-      new Position(placement || "top").negate()};
-    opacity: ${({ show }) => (show ? 1 : 0)};
     padding: 6px 8px;
     border-radius: 4px;
     font-size: 12px;
     color: white;
     background-color: rgba(0, 0, 0, 0.8);
-    letter-spacing: 0.02rem;
+    letter-spacing: 1px;
   }
 `;
